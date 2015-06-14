7ec7dcf2a94eb3985288f46e6504285ce78e677a
David Piggott <david@piggott.me.uk>
2015-06-14 09:24:00 +0100

    Allow JsResultUniformity instantiation in downstream project(s)
    Also switch from Java to Scala type parameter naming conventions (start at A,
    not T, as per http://docs.scala-lang.org/style/naming-conventions.html#type-parameters-generics).
    
    Downstream projects may not need or want to mix-in FormatBehaviours but may
    want to use JsResultUniformity. Moving it to a separate class allows this.

---
+++ src/test/scala/com/dhpcs/json/JsResultUniformity.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 David Piggott <https://www.dhpcs.com>
+ */
+package com.dhpcs.json
+
+import org.scalactic.Uniformity
+import play.api.libs.json.{JsError, JsResult}
+
+class JsResultUniformity[A] extends Uniformity[JsResult[A]] {
+
+  override def normalizedCanHandle(b: Any) = b match {
+    case _: JsError => true
+    case _ => false
+  }
+
+  override def normalizedOrSame(b: Any) = b match {
+    case jsError: JsError => normalized(jsError)
+    case _ => b
+  }
+
+  override def normalized(a: JsResult[A]) = a match {
+    case jsError: JsError =>
+      jsError.copy(
+        errors = jsError.errors.sortBy { case (jsPath, _) => jsPath.toJsonString }
+      )
+    case _ => a
+  }
+
+}
