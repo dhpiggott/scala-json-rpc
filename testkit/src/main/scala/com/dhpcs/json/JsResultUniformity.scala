c941ca943f5936c14368b87dd36b298899f944d2
David Piggott <david@piggott.me.uk>
2016-09-04 17:17:52 +0100

    Simplify JsResultUniformity

--- testkit/src/main/scala/com/dhpcs/json/JsResultUniformity.scala
+++ testkit/src/main/scala/com/dhpcs/json/JsResultUniformity.scala
@@ -4,21 +4,18 @@ import org.scalactic.Uniformity
 import play.api.libs.json.{JsError, JsResult}
 
 class JsResultUniformity[A] extends Uniformity[JsResult[A]] {
-  override def normalizedCanHandle(b: Any) = b match {
-    case _: JsError => true
-    case _ => false
-  }
-
-  override def normalizedOrSame(b: Any) = b match {
-    case jsError: JsError => normalized(jsError)
-    case _ => b
-  }
-
-  override def normalized(aJsResult: JsResult[A]) = aJsResult match {
-    case jsError: JsError =>
-      jsError.copy(
-        errors = jsError.errors.sortBy { case (jsPath, _) => jsPath.toJsonString }
+  override def normalized(jsResult: JsResult[A]): JsResult[A] = jsResult match {
+    case JsError(errors) =>
+      JsError(
+        errors.sortBy { case (jsPath, _) => jsPath.toJsonString }
       )
-    case _ => aJsResult
+    case _ => jsResult
+  }
+
+  override def normalizedCanHandle(o: Any): Boolean = o.isInstanceOf[JsError]
+
+  override def normalizedOrSame(o: Any): Any = o match {
+    case jsError: JsError => normalized(jsError)
+    case _ => o
   }
 }
