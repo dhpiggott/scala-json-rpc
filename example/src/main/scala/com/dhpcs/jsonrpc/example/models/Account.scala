04422cf5a72783b581ed1c9f1fd5fddfe1a72457
David Piggott <david@piggott.me.uk>
2017-02-01 21:47:12 +0000

    Add explicit result type on public field missed by 8062a

--- example/src/main/scala/com/dhpcs/jsonrpc/example/models/Account.scala
+++ example/src/main/scala/com/dhpcs/jsonrpc/example/models/Account.scala
@@ -5,5 +5,5 @@ import play.api.libs.json._
 case class Account(id: Int, name: Option[String] = None, metadata: Option[JsObject] = None)
 
 object Account {
-  implicit val AccountFormat = Json.format[Account]
+  implicit val AccountFormat: Format[Account] = Json.format[Account]
 }
